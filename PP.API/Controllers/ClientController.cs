5e9d98de8df77e31aa04c65c541ab6b22065895d
TrentFetz <tfetzer20@gmail.com>
2023-07-28 23:11:26 -0400

    PracticePanther code for cop4870, Getting started on getting connected to API

---
+++ PP.API/Controllers/ClientController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PP.API.Database;
+
+
+namespace PP.Library.Models
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClientController : ControllerBase
+    {
+        private readonly ILogger<ClientController> _logger;
+
+        public ClientController(ILogger<ClientController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public List<Client> Get()
+        {
+            return Database.Clients;
+        }
+
+
+        [HttpGet("/{id}")]
+        public Client GetId(int id)
+        {
+            return Database.Clients.FirstOrDefault(c=> c.Id == id) ?? new Client();
+        }
+
+
+        [HttpDelete("Delete/{id}")]
+        public Client? Delete(int id)
+        {
+            return new ClientEC().Delete(id);
+        }
+
+        //public Client? Delete(int id)
+        //{
+        //    //return new ClientEC().Delete(client);
+        //    var clientToDelete = Database.Clients.FirstOrDefault(c => c.Id == id);
+        //    if (clientToDelete != null)
+        //    {
+        //        Database.Clients.Remove(clientToDelete);
+        //    }
+        //    return clientToDelete;
+        //}
+
+        [HttpPost]
+        public Client AddOrUpdate([FromBody]Client client)
+        {
+            return new ClientEC().AddOrUpdate(client);
+
+        }
+
+    }
+}
---
+++ PP.API/EC/ClientEC.cs
@@ -0,0 +1,36 @@
+﻿using PP.Library.Models;
+using PP.API.Database;
+
+namespace PP.API.Database
+{
+    public class ClientEC
+    {
+        public Client AddOrUpdate(Client client)
+        {
+            if (client.Id > 0)
+            {
+                var clientToUpdate
+                    = Database.Clients
+                    .FirstOrDefault(c => c.Id == client.Id);
+            }
+            else
+            {
+                client.Id = Database.LastId + 1;
+                Database.Clients.Add(client);
+            }
+            return client;
+        }
+
+        public Client Delete(int id)
+        {
+            var clientToDelete = Database.Clients.FirstOrDefault(c => c.Id == id);
+            if (clientToDelete != null)
+            {
+                Database.Clients.Remove(clientToDelete);
+            }
+            return clientToDelete;
+        }
+
+
+    }
+}
